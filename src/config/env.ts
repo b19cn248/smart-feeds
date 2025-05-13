39b9188597769f045d9ae9ebc3409c1fbcf97e8c
hieunguyen <hieunguyen@vnpt.vn>
2025-05-13 10:30:22 +0700

    refactor code

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 // src/config/env.ts
 export const env = {
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://smart.feeds.gcp.api.openlearnhub.io.vn/',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://smart.feeds.gcp.api.openlearnhub.io.vn',
     API_VERSION: process.env.REACT_APP_API_VERSION || 'v1',
 };
 
