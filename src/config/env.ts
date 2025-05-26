398a96c8b1e322e6b98eb3a5233fce0f14630cad
AnhTu <nguyen.anh.tu.dev@gmail.com>
2025-05-26 10:37:45 +0700

    refactor

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 // src/config/env.ts
 export const env = {
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://smart.feeds.api.openlearnhub.io.vn',
-    // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8888',
+    // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:88888',
 
     API_VERSION: process.env.REACT_APP_API_VERSION || 'v1',
 };
